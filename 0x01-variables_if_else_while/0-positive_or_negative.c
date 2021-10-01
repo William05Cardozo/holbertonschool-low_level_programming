4c47e2083667b20d845af505f45a14f64b0f296b
William05Cardozo <3784@holbertonschool.com>
2021-10-01 13:12:48 -0700

    PositiveorNegaive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - punto de entrada
+ * main - Entry point
  * Descripcion: Si el numero es mayor a 0 es Positivo
  * Si es numero es menor a 0 es Negativo
- * Return: siempre 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -16,8 +16,8 @@ int main(void)
 	if (n > 0)
 		printf("%d is positive\n", n);
 	else if (n == 0)
-		printf("%d is Zero\n", n);
-	else 
+		printf("%d is zero\n", n);
+	else
 		printf("%d is negative\n", n);
 	return (0);
 }
