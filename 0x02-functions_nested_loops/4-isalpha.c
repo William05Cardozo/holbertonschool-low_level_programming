783ecf1aa6a9c5be5f559d9ec35f264e100be318
William05Cardozo <3784@holbertonschool.com>
2021-10-05 09:06:06 -0700

    Isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,14 +8,17 @@
 
 int _isalpha(int c)
 {
-
-	if (c != 97 && c != 122)
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 	{
-		return (1);
+		
+			return (1);
+		
 	}
 
 	else
 	{
 		return (0);
 	}
+
+	_putchar('\n');
 }
