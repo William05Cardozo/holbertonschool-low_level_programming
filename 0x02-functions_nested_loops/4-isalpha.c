4ab73e8b996605fb47f265c7072c1b0a89938e57
William05Cardozo <3784@holbertonschool.com>
2021-10-05 08:33:38 -0700

    Isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isalpha - entry point
+ * @c: variable
+ * Return: 0
+ */
+
+int _isalpha(int c)
+{
+
+	if (c != 97 && c != 122)
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+}
