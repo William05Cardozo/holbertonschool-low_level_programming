af976d4fbfa4fcfb380a172b08c9b4d07a519486
William05Cardozo <3784@holbertonschool.com>
2021-10-06 14:17:14 -0700

    Times

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * times_table - Entry Point
+ * i: Variable
+ * a: Variable
+ * c: Variable
+ * Return: Tabla del 9
+ */
+
+void times_table(void)
+{
+	int i, a, c = 0;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (a = 0; a <= 9; a++)
+		{
+			c = i * a;
+
+			if (c < 10)
+
+			{
+				_putchar(c + '0');
+				_putchar(' ');
+			}
+
+			if (c > 9)
+			{
+				_putchar((c / 10) + '0');
+				_putchar(c % 10 + '0');
+			}
+
+			if (a < 9)
+			{
+				_putchar(';');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
