575ec3f7e1d7272ce47e2c29d5d367805e7039c2
William05Cardozo <3784@holbertonschool.com>
2021-10-07 22:08:34 -0700

    Isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,18 +3,17 @@
 /**
  * _isupper - Entry Point
  * @c: Variable
- * Return: 0 or 1
+ * Return: 1 if c is uppercase
  */
 
 int _isupper(int c)
 {
-	if (c >= 97 && c <= 122)
-	{
-		return (0);
-	}
-	else
+	if (c >= 65 && c <= 90)
 	{
 		return (1);
 	}
+	else
+	{
+		return (0);
+	}
 }
-
