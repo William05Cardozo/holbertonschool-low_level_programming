8d0ac7f741492dd773f01d600b37dd1c3dc3cf69
William05Cardozo <3784@holbertonschool.com>
2021-10-07 22:33:21 -0700

    Mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - Multiplicate
+ * @a: Variable 1
+ * @b: Variable 2
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	printf("%d", a * b);
+
+	return (0);
+}
