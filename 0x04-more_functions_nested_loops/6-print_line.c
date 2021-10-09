af035a63256f07283f0ee0c89762c77b46098130
William05Cardozo <3784@holbertonschool.com>
2021-10-08 20:30:14 -0700

    printLine

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,18 +2,19 @@
 /**
  * print_line - Entry Point
  * a: Variable
+ * @n: Variable
  * Return: New Line
  */
 void print_line(int n)
 {
 	int a;
+
 	a = 0;
 
 	while (n > a)
 	{
 		_putchar('_');
 		a++;
-	
 	}
 	_putchar('\n');
 }
