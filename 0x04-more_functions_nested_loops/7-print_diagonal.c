d6a0230f52aae171f07eae14a245efb8ddb6731b
William05Cardozo <3784@holbertonschool.com>
2021-10-09 21:28:12 -0700

    PrintDiagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Entry Point
+ * @n: Variable
+ * a: Variable
+ * b: Variable
+ * Raturn: No
+ */
+void print_diagonal(int n)
+{
+	int a;
+	int b;
+
+	a = 0;
+
+	while (n > 0)
+	{
+		b = a;
+		while (b > 0)
+		{
+			_putchar(' ');
+			b--;
+		}
+		_putchar(92);
+		_putchar('\n');
+		a++;
+		n--;
+	}
+}
