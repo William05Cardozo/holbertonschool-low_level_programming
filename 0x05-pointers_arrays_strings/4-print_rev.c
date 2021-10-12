9d7f9c4b26c01660aad1f2f5208e17d9a94fab8f
William05Cardozo <3784@holbertonschool.com>
2021-10-11 22:47:05 -0700

    PrintRev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_rev - Entry Point
@@ -13,8 +14,8 @@ void print_rev(char *s)
 
 	for (a = 62; a >= 0; a--)
 	{
-		_putchar(s[a]);
+		putchar(s[a]);
 	}
-	_putchar('\n');
+	putchar('\n');
 
 }
