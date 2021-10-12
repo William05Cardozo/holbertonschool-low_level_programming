545d561859f8efb2ea96076aa948f30a28a1f63a
William05Cardozo <3784@holbertonschool.com>
2021-10-11 20:51:53 -0700

    Swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - Entry Point
+ * @a: Variable
+ * @b: Variable
+ * Return: no line
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
