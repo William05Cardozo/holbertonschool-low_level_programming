4447d36d1220ab532cab0a3287f170409d6771df
William05Cardozo <3784@holbertonschool.com>
2021-10-11 20:38:48 -0700

    Pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - Reset number 98
+ * @n: Variable
+ * Return: No line
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+Comienzo del proyecto 0x05. C - Pointers, arrays and strings
+11/10/2021
+
