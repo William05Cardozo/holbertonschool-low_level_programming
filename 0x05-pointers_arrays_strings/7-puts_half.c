0f995e08bd9b13961aed069634ac75fb7185c17d
William05Cardozo <3784@holbertonschool.com>
2021-10-12 17:37:34 -0700

    PutsHalf

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -4,11 +4,16 @@
 /**
  * puts_half - Entry Point
  * @str: Variable
+ * a: Variable
  * Return: No line
  */
 
 void puts_half(char *str)
 {
+	int a;
+
+	for (a = 0; str[a] != '\0'; a++)
+				;
 	printf("%s", str + 5);
 
 	printf("\n");
