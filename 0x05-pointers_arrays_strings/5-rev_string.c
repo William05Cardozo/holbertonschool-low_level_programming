239deb85d4ded60aef175d8b6d39b03af21e2eb5
William05Cardozo <3784@holbertonschool.com>
2021-10-12 20:37:20 -0700

    RevString

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * rev_string - entry point
+ * @s: Variable
+ * a: Variable
+ * b: Variable
+ * Return: No line
+ */
+
+void rev_string(char *s)
+{
+	int a, b;
+	char str[20];
+
+	a = 0;
+	b = 0;
+
+	while (s[a] != '\0')
+	{
+		str[a] = s[a];
+		a++;
+	}
+	a = a - 1;
+
+	while (a >= 0)
+	{
+		s[a] = str[b];
+		b++;
+		a--;
+	}
+}
