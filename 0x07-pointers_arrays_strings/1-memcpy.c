dca285a93fbff6ddb3f038cca34847bab736b067
William05Cardozo <3784@holbertonschool.com>
2021-10-18 20:21:38 -0700

    Memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _memcpy - Copy memorie
+ * @dest: Destiny of copy
+ * @src: Pointer
+ * @n: Variable
+ * a: Varaible
+ * Return: Dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int a;
+
+	for (a = 0; n > 0 && src[a]; a++)
+	{
+	src[a] = dest[a];
+	}
+	return (dest);
+}
