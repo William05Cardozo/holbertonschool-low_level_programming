73510f4012732ebc1234a8220e70cb95cea9e436
William05Cardozo <3784@holbertonschool.com>
2021-10-19 13:40:05 -0700

    Strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strstr - Entry Point
+ * @haystack: Pointer
+ * @needle: Pointer
+ * Return: NULL
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int a = 0;
+	unsigned int b = 0;
+
+	for (a = 0; haystack[a] != '\0'; a++)
+	{
+		for (b = 0; needle[b] != '\0'; b++)
+		{
+		if (haystack[a] != needle[b])
+		return (needle + b);
+		}
+	}
+	return (NULL);
+}
