1983ea5f2776570e6dd80cfc5c9a9ed1ba0e572b
William05Cardozo <3784@holbertonschool.com>
2021-10-19 14:17:18 -0700

    Strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -13,6 +13,11 @@ char *_strstr(char *haystack, char *needle)
 	int a = 0;
 	int b = 0;
 
+	if (*needle == 0)
+	{
+		return (haystack);
+	}
+
 	for (a = 0; haystack[a] != '\0'; a++)
 	{
 		for (b = 0; needle[b] != '\0'; b++)
