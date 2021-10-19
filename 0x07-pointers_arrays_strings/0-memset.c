9bd775c0b36d241c80f5693a37005b335bff6e38
William05Cardozo <3784@holbertonschool.com>
2021-10-18 17:28:44 -0700

    Pointers-Arrays-Strings

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _memset - Entry Point
+ * @s: Pointer
+ * @b: Variable
+ * @n: Variable
+ * a: Varaible
+ * Return: No line
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int c;
+
+	for (c = 0; c < n; a++)
+	{
+		s[c] = b;
+	}
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+Projecto 0<07 Even mor pointers, arrays and strings
+
+18/10/2021 - 6:19pm
