b2bced2055dc416a0f2cdf780b3f5c16eda3ea13
William05Cardozo <3784@holbertonschool.com>
2021-10-22 11:23:26 -0700

    Whatismyname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,14 @@
 #include "main.h"
 #include <stdio.h>
 
-int main (int __attribute__((unused)) argc, char *argv[])
+/**
+ * main - principal funtion
+ * @argc: Int que cuenta los strings
+ * @argv: Direccion
+ * Return: 0
+ */
+
+int main(int __attribute__((unused)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
