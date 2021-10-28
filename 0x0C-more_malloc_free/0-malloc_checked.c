89c6762998d196647192863807afc4ed5bd5231a
William05Cardozo <3784@holbertonschool.com>
2021-10-28 10:08:16 -0700

    MoreMalloc-Free

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocate memory
+ * @b: Variable
+ * Return: a
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int a;
+
+	a = malloc(sizeof(b));
+
+	if (a == NULL)
+	{
+		exit(98);
+	}
+	return (a);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,2 @@
+Project 0x0C More Malloc and Free
+28/20/2021 - 11:46
