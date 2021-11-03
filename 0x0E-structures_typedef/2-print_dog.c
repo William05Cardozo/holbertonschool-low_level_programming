c046efc08eaefa7e5e206cad81480f1e9fc573bf
William05Cardozo <3784@holbertonschool.com>
2021-11-02 17:13:37 -0700

    PrintDog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,45 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_dog - Init Structure
+ * @d: Pointer
+ * Return: Noline
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		if (d->name == NULL)
+		{
+			printf("Name: (nil)\n");
+		}
+
+		else
+		{
+			printf("Name: %s\n", d->name);
+		}
+
+		if (d->age == 0)
+		{
+			printf("Age: (nil)\n");
+		}
+
+		else
+		{
+			printf("Age: %f\n", d->age);
+		}
+
+		if (d->owner == NULL)
+		{
+			printf("Owner: (nil)\n");
+		}
+
+		else
+		{
+			printf("Owner: %s\n", d->owner);
+		}
+	}
+}
