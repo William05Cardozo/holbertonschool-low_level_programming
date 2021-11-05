b608a5638cf51564d2e777e51f31378d2b436826
William05Cardozo <3784@holbertonschool.com>
2021-11-04 20:44:38 -0700

    Print-Number

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - Function
+ * @separator: string
+ * @n: number of strings
+ * Return: No line
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int a;
+	unsigned int num;
+	va_list list;
+
+	va_start(list, n);
+
+	for (a = 0; a < n; a++)
+	{
+		num = va_arg(list, int);
+		printf("%d\n", num);
+
+		if (separator != NULL)
+		{
+		printf("%s\n", separator);
+		}
+		
+	}
+	printf("\n");
+	va_end(list);
+}
