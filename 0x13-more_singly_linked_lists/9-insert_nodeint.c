d681ac982e1dd2485a4d448c0494b8165323fba8
William05Cardozo <3784@holbertonschool.com>
2021-11-16 19:22:26 -0800

    InsertNodeInt

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,7 +31,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (idx != 0)
 	{
 		tour = *head;
-		for (i = 0; i < idx && tour != NULL; i++)
+		for (i = 1; i < idx && tour != NULL; i++)
 		{
 			tour = tour->next;
 		}
