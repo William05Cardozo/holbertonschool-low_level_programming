9e37aed724c2dae3f7bc8558ecbb4a2f737424fc
William05Cardozo <3784@holbertonschool.com>
2021-11-22 23:00:53 -0800

    Filei-o

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * read_textfile - Function Only read
+ * @filename: File to read
+ * @letters: numbers of letters to read
+ * i: Iterator
+ * Return: read
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int i;
+	ssize_t rd;
+	char *buff;
+
+	buff = malloc(sizeof(char) * letters);
+	if (buff == NULL)
+	{
+		return (0);
+	}
+	i = open(filename, O_RDONLY);
+	if (i == -1)
+	{
+		return (0);
+	}
+	rd = read(i, buff, letters);
+	write(STDOUT_FILENO, buff, rd);
+	close(i);
+	free(buff);
+	return (rd);
+}
+
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,2 @@
+Project 0x15 File I/O 10:30pm 22/11/2.021
+
