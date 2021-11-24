ecddbec86a94d58b5eec9ad5eb9f2dec9376164e
William05Cardozo <3784@holbertonschool.com>
2021-11-23 17:17:04 -0800

    CP

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -17,29 +17,21 @@ int main(int argc, char *argv[])
 	ssize_t rd;
 
 	if (argc != 3)
-	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
-	}
 	inff = open(argv[1], O_RDONLY);
 	if (inff == -1)
-	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
-	}
 	ouft = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (ouft == -1)
-	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
-	}
 	while ((rd = read(inff, buf, 1024)) > 0)
 	{
 		if (write(ouft, buf, rd) != rd)
-		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
-		}
 	}
 	if (rd == -1)
 	{
