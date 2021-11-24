e6b9a2f66058569c293215913d610a48e9482461
William05Cardozo <3784@holbertonschool.com>
2021-11-23 17:24:07 -0800

    CP

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -20,7 +20,8 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	inff = open(argv[1], O_RDONLY);
 	if (inff == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+	exit(98);
 	ouft = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (ouft == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
