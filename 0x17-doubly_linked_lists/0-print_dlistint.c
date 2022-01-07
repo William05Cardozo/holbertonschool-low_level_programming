120e9fe6a9fa7819972af93f9a6347f9164012f4
William05Cardozo <3784@holbertonschool.com>
2022-01-07 15:40:58 -0800

    NewProject

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - Function Print
+ * @h: Pointer
+ * nnode: Number of nodes
+ * Return: nnode
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t nnode;
+
+	for (nnode = 0; h != NULL; nnode++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return(nnode);
+}
