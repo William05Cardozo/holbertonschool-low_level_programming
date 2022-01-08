ddca1d01bfbf0e8b01b50b3002afb7563667f12b
William05Cardozo <3784@holbertonschool.com>
2022-01-07 17:07:48 -0800

    2-Add-Node

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -22,5 +22,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	nnode->prev = NULL;
 	nnode->next = *head;
 	*head = nnode;
+	if (nnode->next != NULL)
+	{
+		nnode->next->prev = nnode;
+	}
 	return (nnode);
 }
