8aa733d5adf7595ee5d6763e5e7ea7a3a3384dce
William05Cardozo <3784@holbertonschool.com>
2022-03-21 22:36:33 -0700

    Island-Perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,19 @@
+#!/bin/python3
+"""
+This function define a perimeter in this island
+"""
+def island_perimeter(grid):
+    width = len(grid[0])
+    height = len(grid)
+    land = 0
+    sea = 0
+
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j ]== 1:
+                land += 1
+                if (i > 0 and grid[i][j -1] == 1):
+                    sea += 1
+                if (j < 0 and grid[i - 1][j] == 1):
+                    sea += 1
+    return land * 4 - sea * 2
