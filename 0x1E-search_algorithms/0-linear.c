c35f1be4093cec90c18c1d2dcadf57bef4501f6c
William05Cardozo <3784@holbertonschool.com>
2022-06-12 18:24:06 -0500

    0x1E: Search_Algorithms, Task 0: 0-Linear, README, Header

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,26 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - Function
+ * @array: is a pointer to the first element of the array to search
+ * @size:  is the number of elements in array
+ * @value: is the value to search
+ * Return: -1 in error case or is NULL
+ **/
+
+
+int binary_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (array == NULL || size == 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (value == array[i])
+			return (i);
+	}
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,9 @@
+#ifdef SEARCH_ALGOS
+#define SEARCH_ALGOS
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Functions */
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+#endif
